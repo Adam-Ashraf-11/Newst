23d0c483d21f015c0faa9671ea423440bed64fce
Adam Ashraf Hamdy <adamashrafhamdy11@gmail>
2026-05-19 07:10:11 +0200

    Add Newst App

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Newst());
+}
+
+class Newst extends StatelessWidget {
+  const Newst({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     
+      
+    );
+  }
+}
