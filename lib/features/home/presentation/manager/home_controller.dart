e497c3195d27959b35c8dbd38defcdc30dc60313
Adam Ashraf Hamdy <adamashrafhamdy11@gmail>
2026-05-26 01:11:51 +0200

    added provider to home view

--- lib/features/home/presentation/manager/home_controller.dart
+++ lib/features/home/presentation/manager/home_controller.dart
@@ -1,15 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:newst_app/core/data_source/services/api_config.dart';
+import 'package:newst_app/core/data_source/services/api_services.dart';
+import 'package:newst_app/features/home/data/models/news_model.dart';
 
 class HomeController extends ChangeNotifier {
+  bool topHeadliesLoading = true;
+  bool everythingLoading = true;
+  String? errorMessage;
+  List<NewsModel> topHeadlinesList = [];
+  List<NewsModel> everythingList = [];
+  ApiServices apiServices = ApiServices();
+  void init() {
+    getEverything();
+    getTopHeadlines();
+  }
 
+  void getTopHeadlines() async {
+    try {
+      topHeadliesLoading;
+      Map<String, dynamic> results = await apiServices.get(
+        endPoints: ApiConfig.topHeadlines,
+        parames: {'country': 'us'},
+      );
+      topHeadlinesList = (results['articles'] as List)
+          .map((e) => NewsModel.fromJson(e))
+          .toList();
+      topHeadliesLoading = false;
+    } catch (e) {
+      topHeadliesLoading = false;
+      errorMessage = e.toString();
+    }
+    notifyListeners();
+  }
 
+  void getEverything() async {
+    try {
+      everythingLoading;
+      Map<String, dynamic> results = await apiServices.get(
+        endPoints: ApiConfig.everything,
+        parames: {'q': 'news'},
+      );
 
-
-
-
-
-
-
-
-
+      everythingList = (results['articles'] as List)
+          .map((e) => NewsModel.fromJson(e))
+          .toList();
+      everythingLoading = false;
+    } catch (e) {
+      everythingLoading = false;
+      errorMessage = e.toString();
+    }
+    notifyListeners();
+  }
 }
--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,94 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:newst_app/core/data_source/services/api_config.dart';
-import 'package:newst_app/core/data_source/services/api_services.dart';
-import 'package:newst_app/features/home/data/models/news_model.dart';
 
-class HomeView extends StatefulWidget {
+import 'package:newst_app/features/home/presentation/manager/home_controller.dart';
+import 'package:provider/provider.dart';
+
+class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
-  @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
-  List<NewsModel> topHeadlinesList = [];
-  List<NewsModel> everythingList = [];
-  ApiServices apiServices = ApiServices();
-  bool isLoading = true;
-  String? errorMessage;
-  @override
-  void initState() {
-    getTopHeadlines();
-    getEverything();
-    super.initState();
-  }
-
-  void getTopHeadlines() async {
-    try {
-      isLoading ;
-      Map<String, dynamic> results = await apiServices.get(
-        endPoints: ApiConfig.topHeadlines,
-        parames: {'country': 'us'},
-      );
-      setState(() {
-        topHeadlinesList = (results['articles'] as List)
-            .map((e) => NewsModel.fromJson(e))
-            .toList();
-        isLoading = false;
-      });
-    } catch (e) {
-      setState(() {
-        isLoading = false;
-        errorMessage = e.toString();
-      });
-    }
-  }
-
-  void getEverything() async {
-    try {
-      isLoading ;
-      Map<String, dynamic> results = await apiServices.get(
-        endPoints: ApiConfig.everything,
-        parames: {'q': 'news'},
-      );
-      setState(() {
-        everythingList = (results['articles'] as List)
-            .map((e) => NewsModel.fromJson(e))
-            .toList();
-        isLoading = false;
-      });
-    } catch (e) {
-      setState(() {
-        isLoading = false;
-        errorMessage = e.toString();
-      });
-    }
-  }
-
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: (errorMessage?.isNotEmpty ?? false)
-          ? Center(child: Text(errorMessage!))
-          : isLoading
-          ? CircularProgressIndicator()
-          : Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: topHeadlinesList.length,
-                    itemBuilder: (context, index) {
-                      return Center(
-                        child: Text(
-                          style: TextStyle(color: Colors.black),
-                          topHeadlinesList[index].title,
+    return ChangeNotifierProvider<HomeController>(
+      create: (BuildContext context) => HomeController()..init(),
+      child: Consumer<HomeController>(
+        builder: (BuildContext context, value, Widget? child) {
+          return Scaffold(
+            body: (value.errorMessage?.isNotEmpty ?? false)
+                ? Center(child: Text(value.errorMessage!))
+                : value.everythingLoading
+                ? Center(child: CircularProgressIndicator())
+                : Column(
+                    children: [
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: value.topHeadlinesList.length,
+                          itemBuilder: (context, index) {
+                            return Center(
+                              child: Text(
+                                style: TextStyle(color: Colors.black),
+                                value.topHeadlinesList[index].title,
+                              ),
+                            );
+                          },
                         ),
-                      );
-                    },
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            ),
+          );
+        },
+      ),
     );
   }
 }
